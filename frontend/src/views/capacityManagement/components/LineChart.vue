ffad70bb8c17a713e13da4096dc2abcd4b11cb1e
SYX2021 <87289377+SYX2021@users.noreply.github.com>
2023-05-09 18:57:33 +0800

    前端修改

---
+++ frontend/src/views/capacityManagement/components/LineChart.vue
@@ -0,0 +1,181 @@
+<template>
+  <div :class="className" :style="{ height: height, width: width }">
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+require('echarts/theme/macarons') // echarts theme
+import resize from '../mixins/resize'
+
+export default {
+  mixins: [resize],
+  props: {
+    className: {
+      type: String,
+      default: 'chart'
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '350px'
+    },
+    autoResize: {
+      type: Boolean,
+      default: true
+    },
+    chartData: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      chart: null
+    }
+  },
+  watch: {
+    chartData: {
+      deep: true,
+      handler(val) {
+        this.setOptions(val)
+      }
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.initChart()
+      console.log(this.chartData)
+    })
+  },
+  beforeDestroy() {
+    if (!this.chart) {
+      return
+    }
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    initChart() {
+      this.chart = echarts.init(this.$el, 'macarons')
+      this.setOptions(this.chartData)
+    },
+    resetZoom() {
+      myChart.setOption({
+        dataZoom: {
+          start: 0,
+          end: 100
+        }
+      });
+    },
+    setOptions({ expectedData, actualData,expectedDataXAxis } = {}) {
+      this.chart.setOption({
+        xAxis: {
+          data: expectedDataXAxis,
+          boundaryGap: false,
+          axisTick: {
+            show: false
+          }
+        },
+        grid: {
+          left: 10,
+          right: 50,
+          bottom: 50,
+          top: 30,
+          containLabel: true
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross'
+          },
+          padding: [5, 10]
+        },
+        yAxis: {
+          axisTick: {
+            show: false
+          }
+        },
+        legend: {
+          data: ['expected', 'actual']
+        },
+        dataZoom: [
+          {
+            type: 'slider',
+            xAxisIndex: 0,
+            start: 0,
+            end: 100,
+            filterMode: 'empty'
+          },
+          {
+            type: 'inside',
+            xAxisIndex: 0,
+            start: 0,
+            end: 100,
+            filterMode: 'empty'
+          },
+          {
+            type: 'slider',
+            yAxisIndex: 0,
+            start: 0,
+            end: 100,
+            filterMode: 'empty'
+          },
+          {
+            type: 'inside',
+            yAxisIndex: 0,
+            start: 0,
+            end: 100,
+            filterMode: 'empty'
+          }]
+        ,
+        series: [{
+          name: 'expected',
+          itemStyle: {
+            normal: {
+              color: '#00D300',
+              lineStyle: {
+                color: '#00D300',
+                width: 1
+              },
+              areaStyle: {
+                color: '#FFFFFF'
+              }
+            }
+          },
+          smooth: true,
+          type: 'line',
+          data: expectedData,
+          animationDuration: 2800,
+          animationEasing: 'cubicInOut'
+        },
+        {
+          name: 'actual',
+          smooth: false,
+          type: 'scatter',
+          symbol: 'circle',
+          symbolSize: 2,
+          itemStyle: {
+            normal: {
+              color:'#B8DEFF',
+            }
+          },
+          emphasis: {
+            symbol: 'triangle',
+            symbolSize: 6
+          },
+          symbol: 'triangle',
+
+          data: actualData,
+          animationDuration: 2800,
+          animationEasing: 'quadraticOut'
+        },
+        ]
+      })
+    }
+  }
+}
+</script>
---
+++ frontend/src/views/capacityManagement/index.vue
@@ -0,0 +1,193 @@
+<template>
+  <div class="dashboard-editor-container">
+    <div class="title">
+      <span class="title-head">容量预测</span>
+      <div class="title-select">
+        <el-dropdown class="avatar-container" trigger="click">
+          <div class="avatar-wrapper">
+            <span class="user-avatar">集群 {{ fileIndex }}</span>
+            <i class="el-icon-caret-bottom"></i>
+          </div>
+          <el-dropdown-menu slot="dropdown" class="user-dropdown">
+            <el-dropdown-item v-for="(filename, index) in this.files" :key="index" @click.native="switchFile(index+1)">{{
+              filename }}</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </div>
+    <el-row class="chart-container">
+      <line-chart :chart-data="lineChartData" />
+    </el-row>
+    <panel-group @handleSetLineChartData="handleSetLineChartData" />
+    
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import PanelGroup from './components/PanelGroup'
+import LineChart from './components/LineChart'
+
+const lineChartData = {
+  allData:{
+    expectedData: [],
+    actualData: [],
+    expectedDataXAxis: []
+  },
+  anomalyData:[
+
+  ]
+}
+
+export default {
+  name: 'DashboardAdmin',
+  components: {
+    PanelGroup,
+    LineChart,
+    AlarmTable
+  },
+  data() {
+    return {
+      fileIndex: 1,
+      files: ['集群1', '集群2', '集群3'],
+      lineChartData: lineChartData.allData,
+      anomalyData:[]
+    }
+  },
+  mounted(){
+    this.$nextTick(() => {
+      this.initData()
+    })
+  },
+  methods: {
+    initData(){
+      this.switchFile(1)
+      this.handleSetLineChartData('futureDay')
+      this.lineChartData = lineChartData.allData
+      console.log(this.lineChartData)
+    },
+    handleSetLineChartData(type) {
+      axios.post(`http://localhost:5000/period`, { time: type }).then(res => {
+        console.log('正在获取目标数据...'),
+        lineChartData.allData.expectedData=res.data.all_data.predict,
+        lineChartData.allData.actualData=res.data.all_data.raw_data,
+        lineChartData.allData.expectedLowerData=res.data.all_data.predict_lower,
+        lineChartData.allData.expectedUpperData=res.data.all_data.predict_upper,
+        lineChartData.allData.actualStatus=res.data.all_data.status,
+        lineChartData.allData.expectedDataXAxis=res.data.all_data.date,
+        console.log('all_data获取成功...')
+      }).catch(error => {
+        if (error.response.status === 400)
+          console.log('出错了')
+      });
+    },
+    switchFile(index) {
+      this.fileIndex = index;
+      console.log('点击了集群 ' + this.fileIndex + ' 的文件');
+      axios.post(`http://localhost:5000/data`, { num: index }).then(res => {
+        console.log('正在获取目标数据...'),
+        lineChartData.allData.expectedData=res.data.all_data.predict,
+        lineChartData.allData.actualData=res.data.all_data.raw_data,
+        lineChartData.allData.expectedLowerData=res.data.all_data.predict_lower,
+        lineChartData.allData.expectedUpperData=res.data.all_data.predict_upper,
+        lineChartData.allData.ActualStatus=res.data.all_data.status,
+        lineChartData.allData.expectedDataXAxis=res.data.all_data.date,
+        console.log('all_data获取成功...'),
+        this.anomalyData=res.data.outliner,
+        console.log('anomaly获取成功...')
+      }).catch(error => {
+        if (error.response.status === 400)
+          console.log('出错了')
+      });
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+}
+
+.chart-container {
+  background-color: #fff;
+  padding: 16px;
+  margin-bottom: 10px;
+}
+
+.title {
+  height: 70px;
+  // overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, .08);
+  padding: 20px;
+
+  .title-head {
+    font-size: 24px;
+    height: 100%;
+    // margin-top: 25px;
+    width: 20%;
+    // background-color: aqua;
+    // border-style: dashed;
+    // border-width: 5px;
+    // border-color: rgb(86, 16, 77);
+  }
+
+  .title-select {
+    float: right;
+    height: 100%;
+    // line-height: 50px;
+    &:focus {
+      outline: none;
+    }
+
+    .avatar-container {
+      margin-right: 30px;
+
+      .avatar-wrapper {
+        // margin-top: 5px;
+        position: relative;
+
+        .user-avatar {
+          font-size: 24px;
+          cursor: pointer;
+          width: 40px;
+          height: 40px;
+          border-radius: 10px;
+        }
+
+        .el-icon-caret-bottom {
+          cursor: pointer;
+          position: absolute;
+          right: -20px;
+          top: 10px;
+          font-size: 12px;
+        }
+
+      }
+
+    }
+
+  }
+}
+.alarm-title{
+  text-align: center;
+  font-size: x-large;
+  height: 70px;
+  // overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, .08);
+  padding: 20px;
+
+}
+.el-dropdown-menu {
+  max-height: 200px;
+  /* 设置最大高度 */
+  overflow-y: scroll;
+  /* 显示垂直滚动条 */
+}
+</style>
